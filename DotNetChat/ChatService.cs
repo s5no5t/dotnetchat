e25fb5b731be9eab4a9c8d58835ca8f65eed28b7
stephan <stephan.oehlert@gmail.com>
2012-09-12 18:26:51 +0200

    Add chat service.

---
+++ DotNetChat/ChatService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Lidgren.Network;
+using DotNetChat.Properties;
+
+namespace DotNetChat
+{
+    class ChatService
+    {
+        private NetPeer _peer;
+        private readonly NetPeerConfiguration _configuration;
+
+        public ChatService(string appIdentifier)
+        {
+            _configuration = new NetPeerConfiguration(appIdentifier);
+            _configuration.EnableMessageType(NetIncomingMessageType.DiscoveryResponse);
+        }
+
+        public void Connect(int port)
+        {
+            _peer = new NetPeer(_configuration);
+            _peer.RegisterReceivedCallback(HandleMessages);
+            _peer.Start();
+            _peer.DiscoverLocalPeers(port);
+        }
+
+        private void HandleMessages(object state)
+        {
+            NetIncomingMessage inc;
+            while ((inc = _peer.ReadMessage()) != null)
+            {
+                switch (inc.MessageType)
+                {
+                    case NetIncomingMessageType.StatusChanged:
+                    case NetIncomingMessageType.DiscoveryRequest:
+                    case NetIncomingMessageType.DiscoveryResponse:
+                        break;
+                }
+            }
+        }
+
+        //private void SendDiscoveryResponse()
+        //{
+        //    NetOutgoingMessage response = _server.CreateMessage();
+        //    response.Write(_serverName);
+        //    response.Write((byte)_clients.Count);
+        //    response.Write(_currentState != GameServerState.Lobby);
+        //    response.WritePadBits(7);
+        //    _server.SendDiscoveryResponse(response, serverMessage.SenderEndpoint);
+        //}
+
+        public void Disconnect()
+        {
+            _peer.Shutdown("good bye");
+        }
+    }
+}
--- DotNetChat/MainWindow.xaml.cs
+++ DotNetChat/MainWindow.xaml.cs
@@ -1,11 +1,11 @@
-﻿
-using DotNetChat.ViewModels;
+﻿using DotNetChat.ViewModels;
 
 namespace DotNetChat
 {
     public partial class MainWindow
     {
         private readonly DotNetChatViewModel _dotNetChatViewModel;
+        private readonly ChatService _chatService;
 
         public MainWindow()
         {
@@ -16,6 +16,20 @@ namespace DotNetChat
             _dotNetChatViewModel.AddChatEntry(new ChatEntryViewModel("Peter", "Hello World"));
 
             DataContext = _dotNetChatViewModel;
+
+            _chatService = new ChatService(Properties.Settings.Default.AppIdentifier);
+            _chatService.Connect(Properties.Settings.Default.Port);
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            _chatService.Disconnect();
         }
     }
 }
